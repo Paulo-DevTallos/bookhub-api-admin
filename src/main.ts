f856f06ae3516a6f0970d626f49e5a0f78ad589c
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-15 15:17:31 -0300

    feat: create initializer server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import { App } from './app';
+const server = new App();
+
+const port = 3006;
+server.initServer(port);
+
+server.app.on('error', onError);
+function onError(error: any): string | number | undefined {
+  if (error.syscall !== 'listen') return error ?? 500;
+  return undefined;
+}
