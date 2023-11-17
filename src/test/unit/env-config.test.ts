7fbabdfdd104185f2553bd53c795f8560947dbd9
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-17 18:57:57 -0300

    test: ensure typeof NODE_ENV is string

--- src/test/unit/env-config.test.ts
+++ src/test/unit/env-config.test.ts
@@ -23,6 +23,12 @@ describe('EnvConfigService', () => {
     expect(typeof port).toBe('number');
   });
 
+  it('ensure typeof NODE_ENV is string', () => {
+    const sut = makeSut();
+    const nodeENv = sut.getNodeEnv();
+    expect(typeof nodeENv).toBe('string');
+  });
+
   it('ensure environment return a value w0hen NODE_ENV is defined', () => {
     const enviromnent = (process.env.NODE_ENV = 'development');
     const sut = makeSut(enviromnent);
