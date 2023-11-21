2fb0b3f3136d7dcbe56ee473f7fd5cdbe5486224
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-20 22:44:14 -0300

    feat: add method to init router and middlewwares

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express, { Application } from 'express';
+import { EmployeesRouter } from './presentation/routers/employees.router';
 
 export class App {
   public app: Application;
+  private employeesRouter = new EmployeesRouter();
 
   constructor(expressInstance: Application = express()) {
     this.app = expressInstance;
+    this.middlewareInitializer();
+    this.initRouter();
+  }
+
+  middlewareInitializer() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  initRouter() {
+    this.app.use(this.employeesRouter.router);
   }
 
   initServer(port: number) {
